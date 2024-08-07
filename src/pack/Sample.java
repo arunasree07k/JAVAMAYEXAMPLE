1ba001f78e0612e410a32db86456c4daf9d85134
aruna <arunasree07k@gmail.com>
2024-08-07 18:21:48 +0530

    first commit

---
+++ src/pack/Sample.java
@@ -0,0 +1,10 @@
+package pack;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		int num1 = 10, num2 = 20;
+		System.out.println("Sum of two numbers=" + (num1 + num2));
+	}
+
+}
