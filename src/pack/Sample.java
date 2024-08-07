2471a7c9a2648f949d3edb5034d68221881b2681
Rahamatulla23 <rahamatullashaik1382@gmail.com>
2024-08-07 19:05:33 +0530

    modified file by rahamatulla

--- src/pack/Sample.java
+++ src/pack/Sample.java
@@ -5,6 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		int num1 = 10, num2 = 20;
 		System.out.println("Sum of two numbers=" + (num1 + num2));
+		System.out.println("Product of two numbers=" + (num1 * num2));
 	}
 
 }
